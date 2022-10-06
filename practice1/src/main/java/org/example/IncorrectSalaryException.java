0d2f371afaf65badc1865ff9de689764e4e63fc6
Vladyslav Bezruk <bezrukvladislav@gmail.com>
2022-10-06 14:51:21 +0300

    Delete practice1 directory

--- practice1/src/main/java/org/example/IncorrectSalaryException.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class IncorrectSalaryException extends Exception {
-    public IncorrectSalaryException() {}
-
-    public IncorrectSalaryException(String message) {
-        super(message);
-    }
-}
